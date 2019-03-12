b75801c911d5004f53542a70956d41d57bc8e949
Andrew Miscoe <armiscoe@gmail.com>
2019-03-12 21:42:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,11 +15,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(i) {
-  let i = 0;
+  function incrementVariable() {
   
     i = i + 1;
     return i;
     }
+}
     do {
       console.log("I run once regardless.");
     }
